62c4794a567892447045937d55f4f4e63c97afa3
haerinkim <37832343+haerinkim@users.noreply.github.com>
2020-12-13 18:33:58 +0900

    Add files via upload

---
+++ TeamProject_1/TeamProject_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeamProject_1
+//
+//  Created by 김해린 on 2020/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeamProject_1/TeamProject_1/TeamProject_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TeamProject_1App.swift
+//  TeamProject_1
+//
+//  Created by 김해린 on 2020/12/13.
+//
+
+import SwiftUI
+
+@main
+struct TeamProject_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
